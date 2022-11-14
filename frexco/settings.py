2dae5ed70a0008e98a99287e99dedfff6810d603
David <david.santos54@fatec.sp.gov.br>
2022-11-14 13:21:33 -0300

    Criação do Model de Usuário e Migração

--- frexco/settings.py
+++ frexco/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DATE_INPUT_FORMATS = ['%d-%m-%Y']
+
+USE_L10N = False
--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Usuario(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    login = models.CharField(max_length=200)
+    senha = models.CharField(max_length=8)
+    dataDeNascimento = models.DateField()
+
+    def gerarSenhaAleatoria(usuario):
+        senha = Usuario.objects.make_random_password(length=8)
+
+    def __str__(self):
+        return self.login
